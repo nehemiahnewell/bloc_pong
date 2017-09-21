1831bad55975a404261c8d67baf8575b3f46f05e
nehemiahnewell <nehemiah.newell@gmail.com>
2017-09-21 03:16:16 +0000

    animated paddle

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,40 +1,96 @@
+//setting up the board goes here
+
 var pong_canvas = document.getElementById('pong_table');
 var size = (pong_canvas.clientWidth > pong_canvas.clientHeight) ? pong_canvas.clientHeight : pong_canvas.clientWidth;
 pong_canvas.width = size;
 pong_canvas.height = size;
 var pong_context = pong_canvas.getContext("2d");
 
-function paddle(direction, position, length) {
-  pong_context.beginPath();
-  pong_context.arc(position, length/2, length/8, 1.5 * Math.PI, .5 * Math.PI, direction);
-  pong_context.lineWidth = 10;
-  pong_context.strokeStyle = 'black';
-  pong_context.stroke();
+function draw_background() {
+  pong_context.fillStyle = 'chartreuse';
+  pong_context.fillRect(0, 0, size, size);
 }
-/*paddle.render = function()
-{
-  pong_context.strokeStyle = 'black';
-  pong_context.stroke();
-};*/
+
+function paddle(position, length) {
+  this.measure = length;
+  this.column = position;
+  this.row = this.measure/2;
+  this.paddle_size = this.measure/10;
+  this.step = length/40;
+  this.move = function(direction)
+  {
+    if(direction === "up")
+    {
+      this.row = this.row - this.step;
+      if(this.row < (this.step * 2))
+      {
+        this.row = (this.step * 2);
+      }
+    }
+    if(direction === "down")
+      {
+      this.row = this.row + this.step;
+      if(this.row > (this.measure - (this.step * 2)))
+      {
+        this.row = (this.measure - (this.step * 2));
+      }
+    }
+  };
+  this.render = function()
+  {
+    pong_context.beginPath();
+    pong_context.moveTo(this.column, (this.row - (this.step * 2)));
+    pong_context.lineTo(this.column, (this.row + (this.step * 2)));
+    pong_context.lineWidth = 10;
+    pong_context.strokeStyle = 'black';
+    pong_context.stroke();
+  };
+}
 
 function pong_ball(position, length) {
-  pong_context.beginPath();
-  pong_context.arc(position, length/2, length/30, 0, 2 * Math.PI, false);
-  pong_context.fillStyle = 'DarkGoldenRod';
-  pong_context.fill();
+  this.measure = length;
+  this.column = position;
+  this.row = this.measure/2;
+  this.step = length/40;
+  this.render = function()
+  {
+    pong_context.beginPath();
+    pong_context.arc(this.column, this.row, this.step, 0, 2 * Math.PI, false);
+    pong_context.fillStyle = 'DarkGoldenRod';
+    pong_context.fill();
+  };
 } 
-/*pong_ball.render = function()
-{
-  pong_context.fillStyle = 'DarkGoldenRod';
-  pong_context.fill();
-};*/
 
-/*function render()
-{
-  var player = new paddle(false,10,size);
-  var enemy = new paddle(true,size - 10,size);
-  var ball = new pong_ball(size/2,size);
-}*/
-var player = new paddle(false,10,size);
-var enemy = new paddle(true,size - 10,size);
+var player = new paddle(10,size);
+var enemy = new paddle(size - 10,size);
 var ball = new pong_ball(size/2,size);
+
+function render ()
+{
+  draw_background();
+  player.render();
+  enemy.render();
+  ball.render();
+}
+// animating the paddle goes here
+
+var animate = window.requestAnimationFrame ||
+              function(callback) { window.setTimeout(callback, 1000/60) };
+
+function step()
+{
+  render();
+  animate(step);
+}
+
+window.addEventListener('keydown',function(e){
+    if(e.keyCode === 38 || e.keyCode === 87)
+    {
+      player.move("up");
+    }
+    if(e.keyCode === 40 || e.keyCode === 83)
+    {
+      player.move("down");
+    }
+  }
+)
